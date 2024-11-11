6b9856268e35308a36a1011827de3907af0e90fb
Douae sb <douae.sb411@gmail.com>
2024-11-11 19:54:00 +0100

    TAX-31-fix-enum-vehicule-type

--- src/main/java/com/taxi/taxista/entity/enums/VehiculeType.java
+++ src/main/java/com/taxi/taxista/entity/enums/VehiculeType.java
@@ -4,14 +4,6 @@ public enum VehiculeType {
 
         SEDAN,
         VAN,
-        MINIBUS;
+        MINIBUS
 
-        public static VehiculeType fromString(String type) {
-                for (VehiculeType v : VehiculeType.values()) {
-                        if (v.name().equalsIgnoreCase(type)) {
-                                return v;
-                        }
-                }
-                throw new IllegalArgumentException("Unknown enum type " + type);
-        }
 }

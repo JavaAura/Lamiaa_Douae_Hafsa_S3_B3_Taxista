e6cd5992e18c6273dfb977aecab07c705bc20219
Douae sb <douae.sb411@gmail.com>
2024-11-11 16:23:09 +0100

    spring boot project

---
+++ src/main/java/com/taxi/taxista/TaxistaApplication.java
@@ -0,0 +1,13 @@
+package com.taxi.taxista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxistaApplication.class, args);
+	}
+
+}

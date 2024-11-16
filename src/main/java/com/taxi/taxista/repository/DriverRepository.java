1378525400ca7f1df5edbda92411152e11a8ac82
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2024-11-16 21:16:37 +0100

    TAX-28 all analytics

--- src/main/java/com/taxi/taxista/repository/DriverRepository.java
+++ src/main/java/com/taxi/taxista/repository/DriverRepository.java
@@ -9,14 +9,5 @@ import java.util.List;
 
 @Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
-//    @Query("SELECT d.id, " +
-//            "SUM(CASE WHEN r.statut = 'CONFIRMÉE' OR r.statut = 'TERMINÉE' " +
-//            "    THEN TIMESTAMPDIFF(HOUR, r.heureDebutCourse, r.heureFinCourse) " +
-//            "    ELSE 0 END) AS totalInCourse, " +
-//            "TIMESTAMPDIFF(HOUR, d.disponibiliteDebut, d.disponibiliteFin) AS totalAvailableHours " +
-//            "FROM Chauffeur d " +
-//            "LEFT JOIN Reservation r ON r.chauffeur.id = d.id " +
-//            "WHERE d.id = :chauffeurId " +
-//            "GROUP BY d.id")
-//    Object[] getOccupationRate(@Param("chauffeurId") Long chauffeurId);
+
 }
--- src/main/java/com/taxi/taxista/service/DriverService.java
+++ src/main/java/com/taxi/taxista/service/DriverService.java
@@ -2,8 +2,12 @@ package com.taxi.taxista.service;
 
 import com.taxi.taxista.DTO.DriverDTO;
 import com.taxi.taxista.entity.Driver;
+import com.taxi.taxista.entity.Reservation;
+import com.taxi.taxista.entity.enums.DriverStatus;
+import com.taxi.taxista.entity.enums.ReservationStatus;
 import com.taxi.taxista.mapper.DriverMapper;
 import com.taxi.taxista.repository.DriverRepository;
+import com.taxi.taxista.repository.ReservationRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +15,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,10 +29,13 @@ public class DriverService {
     private final DriverRepository driverRepository;
     private final DriverMapper driverMapper;
 
+    private final  ReservationRepository reservationRepository;
+
     @Autowired
-    public DriverService(DriverRepository driverRepository, DriverMapper driverMapper) {
+    public DriverService(DriverRepository driverRepository, DriverMapper driverMapper, ReservationRepository reservationRepository) {
         this.driverRepository = driverRepository;
         this.driverMapper = driverMapper;
+        this.reservationRepository = reservationRepository;
     }
 
     public DriverDTO saveDriver(@Valid DriverDTO driverDTO) {
@@ -69,16 +80,40 @@ public class DriverService {
         driverRepository.deleteById(id);
     }
 
-//    public Double getOccupationRate(Long chauffeurId) {
-//        Object[] result = chauffeurRepository.getOccupationRate(chauffeurId);
-//
-//        Double totalInCourse = (Double) result[0];  // Total hours driver was EN_COURSE
-//        Double totalAvailableHours = (Double) result[1];  // Total available hours for the driver
-//
-//        if (totalAvailableHours == 0) {
-//            return 0.0;  // To avoid division by zero if the driver has no available hours
-//        }
-//
-//        return (totalInCourse / totalAvailableHours) * 100;  // Calculate Occupation Rate as percentage
-//    }
+    public double calculateOccupancyPercentage(List<Driver> allDrivers) {
+        long totalNumberOfDrivers = allDrivers.size();
+        long driversCurrentlyOnTrip = allDrivers.stream()
+                .filter(driver -> driver.getStatus() == DriverStatus.ON_TRIP)
+                .count();
+
+        if (totalNumberOfDrivers == 0) {
+            return 0;
+        }
+        double occupancyPercentage = (double) driversCurrentlyOnTrip / totalNumberOfDrivers * 100;
+        return occupancyPercentage;
+    }
+
+    public Map<String, LocalTime[]> calculateAvailabilityTimeSlots(List<Driver> drivers) {
+        Map<String, LocalTime[]> availabilityTimeSlots = new HashMap<>();
+
+        for (Driver driver : drivers) {
+            String driverName = driver.getFirstname();
+            LocalTime[] availability = { driver.getAvailabilityStart(), driver.getAvailabilityEnd()};
+            availabilityTimeSlots.put(driverName, availability);
+        }
+
+        return availabilityTimeSlots;
+    }
+    public Map<String, Long> calculateDriverStatusDistribution(List<Driver> drivers) {
+        Map<String, Long> statusDistribution = new HashMap<>();
+
+        for (Driver driver : drivers) {
+            DriverStatus status = driver.getStatus();
+
+            statusDistribution.put(status.name(),
+                    statusDistribution.getOrDefault(status.name(), 0L) + 1);
+        }
+
+        return statusDistribution;
+    }
 }

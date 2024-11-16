a08e8e5a288b24efaaadeee73f04ede6e2aeeab7
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-11-16 23:58:04 +0100

    yaml modif

--- src/main/java/com/taxi/taxista/mapper/ReservationMapper.java
+++ src/main/java/com/taxi/taxista/mapper/ReservationMapper.java
@@ -3,12 +3,27 @@ package com.taxi.taxista.mapper;
 import com.taxi.taxista.DTO.ReservationDTO;
 import com.taxi.taxista.entity.Reservation;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
+@Component
 public interface ReservationMapper {
 
-    ReservationMapper INSTANCE = Mappers.getMapper(ReservationMapper.class);
+    @Mapping(target = "driverId", source = "driver.id")
+    @Mapping(target = "vehiculeId", source = "vehicule.id")
+    @Mapping(target = "customerId", source = "customer.id")
     ReservationDTO toDto(Reservation reservation);
-    Reservation toEntity(ReservationDTO reservationDTO);
+
+    @Mapping(target = "driver.id", source = "driverId")
+    @Mapping(target = "vehicule.id", source = "vehiculeId")
+    @Mapping(target = "customer.id", source = "customerId")
+    Reservation toEntity(ReservationDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "driver.id", source = "driverId")
+    @Mapping(target = "vehicule.id", source = "vehiculeId")
+    @Mapping(target = "customer.id", source = "customerId")
+    void updateEntityFromDto(ReservationDTO dto, @MappingTarget Reservation entity);
 }
